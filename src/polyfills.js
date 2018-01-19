82886ca0e96763d48be6e3744aa74269ea6684cb
travisxu <xuzhengwei.amor@gmail.com>
2018-01-19 19:26:56 +0800

    Add TAF env support

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,4 +1,7 @@
 /* eslint-disable global-require */
+if (process.env.TAF_CONFIG) {
+  process.env.NODE_ENV = 'production';
+}
 if (process.env.NODE_ENV === 'production') {
   require('source-map-support/register');
 }
